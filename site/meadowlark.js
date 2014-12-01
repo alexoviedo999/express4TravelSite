0ff054588f799a167171a4ca0bac1ac266025307
alexoviedo <alexoviedo999@gmail.com>
2014-11-30 22:04:41 -0500

    added bootstrap.  added new user form. added file upload form.

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -14,12 +14,14 @@ var handlebars = require('express3-handlebars').create({
         }
     }
 });
+var formidable = require('formidable');
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
+app.use(require('body-parser')());
 
 // set 'showTests' context property if the querystring contains test=1
 app.use(function(req, res, next) {
@@ -94,6 +96,59 @@ app.get('/data/nursery-rhyme', function(req, res) {
         noun: 'heck',
     });
 });
+
+app.get('/thank-you', function(req, res){
+	res.render('thank-you');
+});
+
+app.get('/newsletter', function(req, res) {
+	// we will learn about CSRF later...for now, we just
+	// provide a dummy value
+	res.render('newsletter', {
+		csrf: 'CSRF token goes here'
+	});
+});
+
+// app.post('/process', function(req, res) {
+// 	console.log('Form (from querystring): ' + req.query.form);
+// 	console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+// 	console.log('Name (from visible form field): ' + req.body.name);
+// 	console.log('Email (from visible form field): ' + req.body.email);
+// 	res.redirect(303, '/thank-you');
+// });
+
+app.post('/process', function(req, res) {
+	if (req.xhr || req.accepts('json,html') === 'json') {
+		// if there were an error, we would send { error: 'error description' }
+		res.send({
+			success: true
+		});
+	} else {
+		// if there were an error, we would redirect to an error page
+		res.redirect(303, '/thank-you');
+	}
+});
+
+app.get('/contest/vacation-photo', function(req, res) {
+	var now = new Date();
+	res.render('contest/vacation-photo', {
+		year: now.getFullYear(),
+		month: now.getMonth()
+	});
+});
+app.post('/contest/vacation-photo/:year/:month', function(req, res) {
+	var form = new formidable.IncomingForm();
+	form.parse(req, function(err, fields, files) {
+		if (err) return res.redirect(303, '/error');
+		console.log('received fields:');
+		console.log(fields);
+		console.log('received files:');
+		console.log(files);
+		res.redirect(303, '/thank-you');
+	});
+});
+
+
 
 // 404 catch-all handler (middleware)
 app.use(function(req, res, next) {
